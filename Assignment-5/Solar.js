fdebce4e97b4c0e61e8ba4be7966f2cd3e9bca9d
jacobbird <32081987+jacobbird@users.noreply.github.com>
2018-02-20 19:56:43 -0800

    Update Solar.js

--- Assignment-5/Solar.js
+++ Assignment-5/Solar.js
@@ -41,7 +41,7 @@ var far = 120;      // far clipping plane's distance
 // Animation variables
 var time = 0.0;      // time, our global time constant, which is 
                      // incremented every frame
-var timeDelta = 0.5; // the amount that time is updated each fraime
+var timeDelta = 0.0001; // the amount that time is updated each fraime
 
 //---------------------------------------------------------------------------
 //
